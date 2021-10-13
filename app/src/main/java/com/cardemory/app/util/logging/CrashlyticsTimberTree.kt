5a86a23ffdf38e204fb4b1fd25688209580718a3
Anatolii <tberchanov@gmail.com>
2021-10-13 10:24:28 +0300

    Updated versions

--- app/src/main/java/com/cardemory/app/util/logging/CrashlyticsTimberTree.kt
+++ app/src/main/java/com/cardemory/app/util/logging/CrashlyticsTimberTree.kt
@@ -1,15 +1,17 @@
 package com.cardemory.app.util.logging
 
 import android.util.Log
-import com.crashlytics.android.Crashlytics
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
 
 class CrashlyticsTimberTree : DebugTree() {
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        Crashlytics.log(priority, tag, message)
+        val throwableMessage = t?.let { "Throwable: $it" } ?: ""
+        Firebase.crashlytics.log("$tag: $message $throwableMessage")
     }
 
     override fun isLoggable(tag: String?, priority: Int): Boolean {
-        return priority >= Log.INFO
+        return priority >= Log.DEBUG
     }
 }
